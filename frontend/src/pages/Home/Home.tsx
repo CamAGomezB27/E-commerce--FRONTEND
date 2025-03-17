c3d0f07bad49262d2337cbfd698611a5c2368c65
Camilo Gomez <camigomezbernal@gmail.com>
2025-03-17 13:20:50 -0500

    Se generan vista de Home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,34 +1,20 @@
 import React from "react";
 import styles from "./Home.module.css";
+import shirtImage from "../../assets/shirt.png";
 
 const Home: React.FC = () => {
     return(
         <div className={styles.home}>
             {/* HERO SECTION */}
             <section className={styles.hero}>
-                <h1> Bienvenid@ a Nuestra Tienda </h1>
-                <p> Descubre nuestras ultimas prendas en tendencia </p>
-                <button className={styles.cta}> Explorar Productos </button>
-            </section>
-
-            {/* Categorias */}
-            <section className={styles.categories}>
-                <h2> Categorias Destacadas </h2>
-                <div className={styles.categoryGrid}>
-                    <div className={styles.categoryCard}> Ropa para Hombre </div>
-                    <div className={styles.categoryCard}> Ropa para Mujer </div>
+                <div className={styles.herotext}>
+                    <h1> Bienvenid@ a Nuestra Tienda </h1>
+                    <button className={styles.cta}> Explorar Productos </button>
                 </div>
-            </section>
-
-            {/* Productos Recomendados */}
-            <section className={styles.featured}>
-                <h2> Productos Recomendados </h2>
-                <div className={styles.categoryGrid}>
-                    <div className={styles.categoryCard}> Producto 1 </div>
-                    <div className={styles.categoryCard}> Producto 2 </div>
-                    <div className={styles.categoryCard}> Producto 3 </div>
+                <div className={styles.heroimage}>
+                <img src={shirtImage} alt="imagen" />
                 </div>
-            </section>
+            </section> 
         </div>
     ) 
 }
